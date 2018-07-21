3ad423f1d17d887db4848e7164e74db9d8d73660
Musa Jahun <msjahun@live.com>
2018-07-21 12:49:03 +0300

    added home activity

---
+++ app/src/main/java/com/example/dc/emu_dormitory_reservation_app/Home_activity/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.dc.emu_dormitory_reservation_app.Home_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
