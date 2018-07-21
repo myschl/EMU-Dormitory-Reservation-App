8eff2f524d53d9afa1ad86d2a43e32133fceb0dd
Musa Jahun <msjahun@live.com>
2018-07-21 11:26:33 +0300

    added the recycleView of the searchResult list Fragment

--- app/src/main/java/com/example/dc/emu_dormitory_reservation_app/search_results_activity/Fragment_list_searchResult.java
+++ app/src/main/java/com/example/dc/emu_dormitory_reservation_app/search_results_activity/Fragment_list_searchResult.java
@@ -1,24 +1,37 @@
 package com.example.dc.emu_dormitory_reservation_app.search_results_activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dc.emu_dormitory_reservation_app.R;
+import com.example.dc.emu_dormitory_reservation_app.rate_bookings_activity_2.RateBookingsDataModel2;
+import com.example.dc.emu_dormitory_reservation_app.rate_bookings_activity_2.RateBookingsRecycleViewAdapter2;
+
+import java.util.ArrayList;
 
 public class Fragment_list_searchResult extends Fragment{
     private static final String TAG = "Fragment_list_searchRes";
+    Context mContext;
+    ArrayList<SearchResultsListDataModel> mSearchResultsListDataModels = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: I am here listSearchResults");
        View view = inflater.inflate(R.layout.fragment_listview_search_result_activity, container, false);
+
+        mContext = getContext();
+        dataInit();
+        initRecycleView(view);
        return view;
     }
 
@@ -32,5 +45,61 @@ public class Fragment_list_searchResult extends Fragment{
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: listSearchResult");
+    }
+
+    private void dataInit(){
+        mSearchResultsListDataModels.add(new SearchResultsListDataModel (
+                "Alfam Dormitories",
+                "Alfam dormitories has four blocks seperate.....",
+                "4.5",
+                "Very Good",
+                "https://dormitories.emu.edu.tr/PublishingImages/Dormitories/alfam/6.jpg",
+                "23"
+        ));
+        mSearchResultsListDataModels.add(new SearchResultsListDataModel (
+                "Alfam Dormitories",
+                "Alfam dormitories has four blocks seperate.....",
+                "4.5",
+                "Very Good",
+                "https://dormitories.emu.edu.tr/PublishingImages/Dormitories/alfam/6.jpg",
+                "23"
+        ));
+        mSearchResultsListDataModels.add(new SearchResultsListDataModel (
+                "Alfam Dormitories",
+                "Alfam dormitories has four blocks seperate.....",
+                "4.5",
+                "Very Good",
+                "https://dormitories.emu.edu.tr/PublishingImages/Dormitories/alfam/6.jpg",
+                "23"
+        ));
+        mSearchResultsListDataModels.add(new SearchResultsListDataModel (
+                "Alfam Dormitories",
+                "Alfam dormitories has four blocks seperate.....",
+                "4.5",
+                "Very Good",
+                "https://dormitories.emu.edu.tr/PublishingImages/Dormitories/alfam/6.jpg",
+                "23"
+        ));
+        mSearchResultsListDataModels.add(new SearchResultsListDataModel (
+                "Alfam Dormitories",
+                "Alfam dormitories has four blocks seperate.....",
+                "4.5",
+                "Very Good",
+                "https://dormitories.emu.edu.tr/PublishingImages/Dormitories/alfam/6.jpg",
+                "23"
+        ));
+
+
+
+
+    }
+
+
+    private void initRecycleView(View v){
+        Log.d(TAG, "initRecyclerView: Called");
+        RecyclerView recycleViewFragment_list_search_result = v.findViewById(R.id.recycleViewSearchResultsList);
+       SearchResultsListRecycleViewAdapter adapter = new SearchResultsListRecycleViewAdapter(mContext, mSearchResultsListDataModels);
+        recycleViewFragment_list_search_result.setAdapter(adapter);
+        recycleViewFragment_list_search_result.setLayoutManager( new LinearLayoutManager(mContext));
     }
 }
---
+++ app/src/main/java/com/example/dc/emu_dormitory_reservation_app/search_results_activity/SearchResultsListRecycleViewAdapter.java
@@ -0,0 +1,95 @@
+package com.example.dc.emu_dormitory_reservation_app.search_results_activity;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.dc.emu_dormitory_reservation_app.R;
+import com.example.dc.emu_dormitory_reservation_app.rate_bookings_activity_2.RateBookingsDataModel2;
+
+import java.util.ArrayList;
+
+public class SearchResultsListRecycleViewAdapter extends RecyclerView.Adapter<SearchResultsListRecycleViewAdapter.ViewHolder>{
+
+
+    private static final String TAG = "SearchResultsListRecycl";
+    Context mContext;
+    ArrayList<SearchResultsListDataModel> mSearchResultsListDataModel = new ArrayList<>();
+
+    public SearchResultsListRecycleViewAdapter(Context mContext, ArrayList<SearchResultsListDataModel> mSearchResultsListDataModel) {
+        this.mContext = mContext;
+        this.mSearchResultsListDataModel = mSearchResultsListDataModel;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: ");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_search_results_list, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+      //  Glide.with(mContext).asBitmap().load(mCategoriesDataModel.get(position).getCategoryImageUrl()).into(holder.imageViewCategories);
+        Glide.with(mContext).asBitmap().load(mSearchResultsListDataModel.get(position).getDormitoryImageUrl()).into(holder.imageViewDormitoryImage);
+
+        // holder.textViewCategoriesTitle.setText(mCategoriesDataModel.get(position).getCategoryTitle());
+
+      //  holder.relativeLayoutCategories.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+         //   public void onClick(View v) {
+         //       Log.d(TAG, "onClick: clicked on: " + mCategoriesDataModel.get(position).getCategoryTitle());
+         //       Toast.makeText(mContext, mCategoriesDataModel.get(position).getCategoryTitle(), Toast.LENGTH_SHORT).show();
+//
+         //   }
+      //  });
+
+
+        holder.textViewDormitoryName.setText(mSearchResultsListDataModel.get(position).getDormitoryName());
+        holder.textViewDormitoryDescription.setText(mSearchResultsListDataModel.get(position).getDormitoryShortDescription());
+
+        holder.textViewRatingValue.setText(mSearchResultsListDataModel.get(position).getDormitoryRating());
+        holder.textViewRatingStatus.setText(mSearchResultsListDataModel.get(position).getDormitoryRatingStatus());
+
+        //  holder.imageViewMoreOptions;
+      //  holder.ralativeLayoutRateBookings2;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSearchResultsListDataModel.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageViewDormitoryImage;
+        TextView textViewDormitoryName;
+        TextView textViewDormitoryDescription;
+        ImageView imageViewMoreOptions;
+        TextView textViewRatingValue;
+        TextView textViewRatingStatus;
+        RelativeLayout ralativeLayoutRateBookings2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageViewDormitoryImage = (ImageView) itemView.findViewById(R.id.imageViewDormitoryImage);
+            textViewDormitoryName =  itemView.findViewById(R.id.textViewDormitoryName);
+            textViewDormitoryDescription = itemView.findViewById(R.id.textViewDormitoryDescription);
+            imageViewMoreOptions = itemView.findViewById(R.id.imageViewMoreOptions);
+            textViewRatingValue = itemView.findViewById(R.id.textViewRatingValue);
+            textViewRatingStatus = itemView.findViewById(R.id.textViewRatingStatus);
+            ralativeLayoutRateBookings2 = itemView.findViewById(R.id.ralativeLayoutRateBookings2);
+        }
+    }
+}
